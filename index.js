2bf14fea2096e441c4538bd2d0365118af64ae3a
Prateek Rai <raiprateek19azm@gmail.com>
2026-06-12 12:22:37 +0530

    Initial commit: Add healthy server JSON response

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.json({
+    message: "This server is healthy!"
+  });
+});
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
